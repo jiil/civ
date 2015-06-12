ff77ff809e7fcfe8d3b07119f838683d093592b9
Hyoun Jiil <jiil.hyoun@samsung.com>
2015-06-12 17:08:00 +0900

    generate parse.js

--- parse.js
+++ parse.js
@@ -2,64 +2,87 @@ var htmlparser = require("htmlparser2");
 var DomUtils = require('domutils');
 var _ = require('underscore');
 var fs = require('fs');
-var handler = new htmlparser.DomHandler(function (error, dom) {
-    if (error){ console.log(error) }
-    else{ 
-        var tabber = DomUtils.getElements({tag:'div', class:'tabber'},dom,true)[0];
+var handler = new htmlparser.DomHandler(function(error, dom) {
+    if (error) {
+        console.log(error)
+    } else {
+        var tabber = DomUtils.getElements({
+            tag: 'table',
+            class: 'wikitable'
+        }, dom, true)[0];
         //var tabber = DomUtils.getElements({tag:'a', class:'mw-redirect'},dom,true)[0];
         //console.log(tabber);
         console.log(JSON.stringify(elm2obj(tabber)));
     }
-}, {normalizeWhitespace:true});
+}, {
+    normalizeWhitespace: true
+});
 var parser = new htmlparser.Parser(handler);
-fs.readFile(__dirname + '/Cathedral_(Civ5)', function(err, result){
-parser.write(result);
-parser.done();
+fs.readFile(__dirname + '/Cathedral_(Civ5)', function(err, result) {
+    parser.write(result);
+    parser.done();
 });
 
-function elm2obj(elm){
+function elm2obj(elm) {
     var obj = {};
     switch (elm.type) {
         case 'tag':
-            if (elm.name) {
-                obj.name = elm.name;
-            }
-            if(elm.attribs.class){obj.name = obj.name + "_" + elm.attribs.class;}
-            var attr = _.omit(elm.attribs, 'class', 'colspan', 'style', 'alt', 'data-image-key', 'data-image-name', 'width', 'height');
-            if (_.keys(attr).length > 0) {
-                obj.attribs = attr;
-            }
-            obj.children = [];
-            _.each(elm.children, function(child) {
-                var cobj = elm2obj(child);
-                if(cobj){
-                    if(cobj.text && obj.children.length > 0 && obj.children[(obj.children.length -1)].text){
-                        obj.children[(obj.children.length -1)].text = obj.children[(obj.children.length -1)].text + " " + cobj.text;
+            switch(elm.name){
+                case 'a' : 
+                    obj = _.map(elm.children, function(child){return elm2obj(child);});
+                    break;
+                case 'img':
+                    if(elm.attribs){
+                        obj = elm.attribs.alt.replace(/^20x/,"").replace(/5$/,"").replace(/ (Civ5)/,"");
                     }else{
-                        obj.children.push(cobj)
-                    }
-                }
-            });
-            if(!obj.attribs){
-                switch(obj.children.length){
-                    case 0 :
                         obj = null;
-                        break;
-                    case 1:
-                        obj = obj.children[0];
-                        break;
-                    default:
-                        break;
-                }
+                    }
+                    break;
+                case 'span' :
+                    if(elm.attribs.style === 'display:none;'){
+                        obj = null;
+                    }else{
+                        obj = _.compact(_.flatten(_.map(elm.children, function(child){return elm2obj(child);})));
+                    }
+                    break;
+                case 'noscript':
+                    obj = null;
+                    break;
+                case 'li':
+                    var sons = _.compact(_.flatten(_.map(elm.children, function(child){return elm2obj(child);})));
+                    if( _.every(sons, function(son){return (typeof son === 'string');})){
+                        obj = sons.join(" ");
+                    }else{
+                        obj.name = elm.name;
+                        obj.children = sons;
+                    }
+                    break;
+                case 'tr':
+                    var sons = _.compact(_.flatten(_.map(elm.children, function(child){return elm2obj(child);})));
+                    obj[_.first(sons)] = _.rest(sons);
+                    break;
+                case 'table':
+                    var sons = _.compact(_.flatten(_.map(elm.children, function(child){return elm2obj(child);})));
+                    obj = _.reduce(sons, function(o,s){return _.extend(o,s)}, {});
+                    break;
+                case 'br':
+                case 'b':
+                case 'i':
+                case 'p':
+                case 'ul':
+                case 'div':
+                case 'th':
+                case 'td':
+                    obj = _.compact(_.flatten(_.map(elm.children, function(child){return elm2obj(child);})));
+                    break;
+                default:
+                    obj.name = elm.name;
+                    obj.children = _.compact(_.flatten(_.map(elm.children, function(child){return elm2obj(child);})));
+                    break;
             }
             break;
         case 'text':
-            var text = elm.data.trim();
-            if (text.length > 0) {
-                obj.text = text;
-            }else{
-                obj = null;
-            }
+            obj = elm.data.trim();
             break;
     }
     //console.log(obj);
